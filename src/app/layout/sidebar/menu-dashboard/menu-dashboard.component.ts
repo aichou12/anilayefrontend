5009524ad9562cb63488ac9377eb890504b031ee
Arona Ndiaye <arona010ndiaye@gmail.com>
2025-09-16 14:50:20 +0000

    Mise à jour dashboard.component + ajout dashboard.service

--- src/app/layout/sidebar/menu-dashboard/menu-dashboard.component.ts
+++ src/app/layout/sidebar/menu-dashboard/menu-dashboard.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
-import { DashboardService } from '../menu-dashboard/dashboard.service';
+import { DashboardService } from './dashboard.service';
 
 @Component({
   selector: 'app-menu-dashboard',
